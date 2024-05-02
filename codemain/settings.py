ab3edb45873b8a2023c23274507934fc0716ae8e
Aaron storey <aaron.storey@hotmail.co.uk>
2024-05-02 17:12:05 +0000

    Feat(heroku)Ready to be deplyed

--- codemain/settings.py
+++ codemain/settings.py
@@ -13,8 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
-if os.path.isfile('env.py'):
-    import env
+if os.path.isfile('env.py'):import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -27,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-vz9gcf1uy@yz-q-^tm0t^+bl$b-)2)%_b4**1fh8%9w6&zcum_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com',]
 
 
 # Application definition
