bc48007eecc0c833f63d318d6ffc344093f81297
Aaron storey <aaron.storey@hotmail.co.uk>
2024-05-02 18:09:01 +0000

    Feat(Models)first models getting made but need to ncheck the types before i migrate

--- game_sessions/models.py
+++ game_sessions/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Post(models.Model):
+    post_id = models.CharField(max_length=200, unique=True)
+    title = models.CharField(max_length=200, unique=True)
+    gamer_tag = models.CharField(max_length=200, unique=True)
+    description = models.CharField(max_length=400, unique=True)
+    datetime = models.DateTimeField()
+    game = models.Choices()
+    player_count = models.IntegerField(max_length=2)
