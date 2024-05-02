a6d57ad67ac136fc0e8869fd20b26f27797bfa45
Aaron storey <aaron.storey@hotmail.co.uk>
2024-05-02 17:26:40 +0000

    Feat(getting heroku ready for the data base)i have made the secret key and added it to the heroku secret key

--- codemain/settings.py
+++ codemain/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-vz9gcf1uy@yz-q-^tm0t^+bl$b-)2)%_b4**1fh8%9w6&zcum_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.herokuapp.com','127.0.0.1']
 
@@ -129,3 +129,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+SECRET_KEY = os.environ.get("SECRET_KEY")
