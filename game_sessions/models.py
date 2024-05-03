e2dc0f012497b58febe9dd8d1dc608b664fdae75
Aaron storey <aaron.storey@hotmail.co.uk>
2024-05-03 22:55:01 +0000

    Feat(post model):this commit is my post migration with game choices

--- game_sessions/models.py
+++ game_sessions/models.py
@@ -3,10 +3,25 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Post(models.Model):
-    post_id = models.CharField(max_length=200, unique=True)
+    """
+    this is the post class this lets admins and users with right authority to 
+    make a post
+    """
+    GAME_CHOICES = (
+        ('Halo', 'Halo'),
+        ('Hell Divers 2', 'Hell Divers 2'),
+        ('VR Chat', 'VR Chat'),
+        ('Call Of Duty', 'Call Of Duty'),
+        ('Other', 'Other'),
+    )
+
+    post_id = models.SlugField(max_length=200, unique=True)
     title = models.CharField(max_length=200, unique=True)
     gamer_tag = models.CharField(max_length=200, unique=True)
-    description = models.CharField(max_length=400, unique=True)
+    description = models.TextField(max_length=500, unique=True)
     datetime = models.DateTimeField()
-    game = models.Choices()
-    player_count = models.IntegerField(max_length=2)
+    game = models.CharField(max_length=20, choices=GAME_CHOICES, default='Other')
+    player_count = models.IntegerField()
+
+    def __str__(self):
+        return self.title
