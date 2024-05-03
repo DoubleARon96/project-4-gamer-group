2f3bbedf146749192f17044fdb09203326166a2f
Aaron storey <aaron.storey@hotmail.co.uk>
2024-05-03 23:03:10 +0000

    Featadmin): this commit i made it so admins can see the model but cant post just yet, also debug is set to False for deployment

--- codemain/settings.py
+++ codemain/settings.py
@@ -89,6 +89,12 @@ DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.codeanyapp.com",
+    "https://*.herokuapp.com",
+    "https://*.www.gitpod.io/"
+]
+
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
