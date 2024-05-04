fbcba7d97e93cc6f95ee6c67a924734ada1cc6e3
Aaron storey <aaron.storey@hotmail.co.uk>
2024-05-04 19:56:47 +0000

    Feat(about)i have added a basic modle to about

--- about/models.py
+++ about/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class AdminStory (models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE,)
+    story_id = models.SlugField(max_length=200, unique=True)
+    stories = models.TextField (unique=True)
+    updated_on = models.DateTimeField(auto_now=True)
