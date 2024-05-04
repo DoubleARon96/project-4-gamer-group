224565739f15e0ebc3bca04dcbcc8f3b3196a002
Aaron storey <aaron.storey@hotmail.co.uk>
2024-05-04 13:21:50 +0000

    Feat(date record)records the date when the post is made

--- game_sessions/models.py
+++ game_sessions/models.py
@@ -22,6 +22,7 @@ class Post(models.Model):
     datetime = models.DateTimeField()
     game = models.CharField(max_length=20, choices=GAME_CHOICES, default='Other')
     player_count = models.IntegerField()
+    updated_on = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
