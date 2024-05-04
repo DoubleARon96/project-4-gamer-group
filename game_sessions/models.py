48c1cad87e82dec8aed2e98466f8d4527936387f
Aaron storey <aaron.storey@hotmail.co.uk>
2024-05-04 18:35:48 +0000

    Feat(Comments)comments made and migrated

--- game_sessions/models.py
+++ game_sessions/models.py
@@ -29,11 +29,9 @@ class Post(models.Model):
         return self.title
 
 
-#class Chat(models.Model):
-#    post = models.ForeignKey(
-#        Post, on_delete=models.CASCADE, related_name="comments")
-#    author = models.ForeignKey(
-#        User, on_delete=models.CASCADE, related_name="commenter")
-#    body = models.TextField()
-#    approved = models.BooleanField(default=False)
-#    created_on = models.DateTimeField(auto_now_add=True)
+class Comments(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="commenter")
+    body = models.TextField()
+    approved = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
