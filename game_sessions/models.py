f5504b4d6d1a7f6b094526d10c8bbc01db798d81
Aaron storey <aaron.storey@hotmail.co.uk>
2024-05-04 14:16:21 +0000

    Feat (Chat)i have made a model for games sessions called chat for when people have joined or have questions about the session they can use it also commented it out because i need to go through it again

--- game_sessions/models.py
+++ game_sessions/models.py
@@ -27,3 +27,13 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+
+#class Comment(models.Model):
+#    post = models.ForeignKey(
+#        Post, on_delete=models.CASCADE, related_name="comments")
+#    author = models.ForeignKey(
+#        User, on_delete=models.CASCADE, related_name="commenter")
+#    body = models.TextField()
+#    approved = models.BooleanField(default=False)
+#    created_on = models.DateTimeField(auto_now_add=True)
