0e0a916914a1cf9de7be65db049e291267b5e346
Aaron storey <aaron.storey@hotmail.co.uk>
2024-05-05 15:13:48 +0000

    Feat(Fixed comment models) this consists of changing a few attributs like author was changed to gamer tag and i added a title too

--- game_sessions/models.py
+++ game_sessions/models.py
@@ -35,7 +35,8 @@ class Post(models.Model):
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="commenter")
+    title = models.CharField(max_length=200, unique=True, default="title")
+    gamer_tag = models.ForeignKey(User, on_delete=models.CASCADE, related_name="commenter")
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
