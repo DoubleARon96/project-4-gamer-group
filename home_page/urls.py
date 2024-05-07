10c306a85e3bedcbfd06f4ad74182dc018f0ded8
Aaron storey <aaron.storey@hotmail.co.uk>
2024-05-07 20:41:24 +0000

    Feat(home)just deleted home/

--- home_page/urls.py
+++ home_page/urls.py
@@ -2,7 +2,7 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('home/', views.Home_Page.as_view(), name='home')
+    path('', views.Home_Page.as_view(), name='home')
     
 ]
 
