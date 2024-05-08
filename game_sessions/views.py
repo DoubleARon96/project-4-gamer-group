337e6375bf406576f52b63b5796a64871fd52774
Aaron storey <aaron.storey@hotmail.co.uk>
2024-05-08 22:59:39 +0000

    Feat(post view):i have made the html and linked it but because i didnt call my post_id, slug it dosnt like it

--- game_sessions/views.py
+++ game_sessions/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from django.views import generic
 from .models import Post
 
@@ -8,3 +8,15 @@ class Game_session_List(generic.ListView):
     #template_name = "post_list.html"
     template_name = "game_sessions/index.html"
     paginate_by = 3
+
+
+    def post_description(request, post_id):
+
+        queryset = Post.objects.filter()
+        post = get_object_or_404(queryset, slug=post_id)
+
+        return render(
+            request,
+            "game_sessions/post_descripton.html",
+            {"post": post},
+    )
