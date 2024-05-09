961ef58ac9ec336d342ad499dfc693b8ddc1e427
Aaron storey <aaron.storey@hotmail.co.uk>
2024-05-09 21:41:51 +0000

    Feat(home view) still working on it but need to save before i head off

--- home_page/urls.py
+++ home_page/urls.py
@@ -1,8 +1,11 @@
 from . import views
 from django.urls import path
+from .views import HomePageView
 
 urlpatterns = [
-    path('', views.HomePage.objects.all, name='home')
+    #path('', HomePageView.as_view(), name='home'),
+    #path('home/', views.HomePageView, name='home')
+    path('home/', HomePageView.as_view(), name='home'),
     
 ]
 
--- home_page/views.py
+++ home_page/views.py
@@ -1,28 +1,32 @@
 from django.shortcuts import render,get_object_or_404
 from django.views import generic
+from django.views.generic.base import TemplateView
 from .models import HomePage
 
-def home_list(request):
-    """
-    Renders the About page
-    """
-    try:
-        home_page = HomePage.objects.get(gamer_tag="Aaron") 
-    except HomePage.DoesNotExist:
-        home_page= get_object_or_404
 
-        content = {
-            "home_page": home_page,
-        }
+class HomePageView(TemplateView):
+    template_name = "home_content_list.html"
 
-        return render(request, "home_page/home_content_list.html", content)
+    def get_content(self, **kwargs):
+        context = super().get_content(all)
+        context["latest_articles"] = HomePage.objects.content()
+        return context
+
+#def home_list(request):
+ #   """
+ #   Renders the About page
+ #   """
+ #   try:
+  #      home_page = HomePage.objects.get(gamer_tag="Aaron") 
+  #  except HomePage.DoesNotExist:
+  #      home_page= get_object_or_404
+#
+ #       content = {
+  #          "home_page": home_page,
+   #     }
+#
+ #       return render(request, "home_page/home_content_list.html", content)
 
 #class Home_Page(generic.View):
 #    queryset =()
 #    template_name = "home_content/index.html"
-
-#class Home_Page_List(View):
-    #template_name = compile("home_page/templates/home_page/home_content_list.html",'<string>', 'exec')
-    #def home(request):
-    #    context = {'message': 'Hello, Django!'}
-    #    return render(request, 'myapp/index.html', context)
