710697de9d941c5b5f21900ece4cfe702645c143
Aaron storey <aaron.storey@hotmail.co.uk>
2024-05-13 22:27:34 +0000

    Feat (home)i have changed the view to a basic code to try and debug and i have also changed the template name for ease of work

--- home_page/views.py
+++ home_page/views.py
@@ -1,11 +1,7 @@
 from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponse
-from django.template import loader
 from django.views import generic
 
 
-class HomePage(generic):
-    
-      def __init__(self):
-          template = loader.get_template('home_content_list.html')
-          return HttpResponse(template.render())
+def HomePageLoader(request):
+    return render(request, 'home_page.html')
