320f3e57dbd33c8e6fbdafc6f704e0d1e011de95
Aaron storey <aaron.storey@hotmail.co.uk>
2024-05-13 21:38:50 +0000

    Feat(Home) tonight i was trying differnt ways to view the home page but still no luck yet

--- home_page/urls.py
+++ home_page/urls.py
@@ -1,11 +1,11 @@
 from . import views
 from django.urls import path
-from .views import HomePageView
+from .views import HomePage
 
 urlpatterns = [
     #path('', HomePageView.as_view(), name='home'),
     #path('home/', views.HomePageView, name='home')
-    path('home/', HomePageView.as_view(), name='home'),
+    path('home/', HomePage.as_view(), name='home'),
     
 ]
 
--- home_page/views.py
+++ home_page/views.py
@@ -1,32 +1,11 @@
 from django.shortcuts import render,get_object_or_404
+from django.http import HttpResponse
+from django.template import loader
 from django.views import generic
-from django.views.generic.base import TemplateView
-from .models import HomePage
 
 
-class HomePageView(TemplateView):
-    template_name = "home_content_list.html"
-
-    def get_content(self, **kwargs):
-        context = super().get_content(all)
-        context["latest_articles"] = HomePage.objects.content()
-        return context
-
-#def home_list(request):
- #   """
- #   Renders the About page
- #   """
- #   try:
-  #      home_page = HomePage.objects.get(gamer_tag="Aaron") 
-  #  except HomePage.DoesNotExist:
-  #      home_page= get_object_or_404
-#
- #       content = {
-  #          "home_page": home_page,
-   #     }
-#
- #       return render(request, "home_page/home_content_list.html", content)
-
-#class Home_Page(generic.View):
-#    queryset =()
-#    template_name = "home_content/index.html"
+class HomePage(generic):
+    
+      def __init__(self):
+          template = loader.get_template('home_content_list.html')
+          return HttpResponse(template.render())
