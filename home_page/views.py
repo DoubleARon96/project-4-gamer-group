f8001f427efeaa910cacd51eb81a7b1d374d5279
Aaron storey <aaron.storey@hotmail.co.uk>
2024-05-15 18:06:59 +0000

    Feat(home)view is printing queryset and nothing else

--- home_page/views.py
+++ home_page/views.py
@@ -2,11 +2,14 @@ from django.shortcuts import render,get_object_or_404
 from .models import HomePage
 
 #class HomePage_View(ListView):
-     
 def indexPage(request):
-     return render(request, "home_page.html")
+    data = HomePage.objects.all()
+    contents = {"contents": data}
+    return render(request, 'home_page.html', contents )
 
 def data_print(request):
     data = HomePage.objects.all()
-    contents = {"home_list": data}
-    return render(request, 'home_page.html', contents)
+    contents = {"contents": data}
+    return render(request, 'home_page.html', {"contents": contents} )
+
+from django.shortcuts import render
