cfc6f79332c9c1771212b3170481a6e97cf2bbc9
Aaron storey <aaron.storey@hotmail.co.uk>
2024-05-16 21:10:57 +0000

    Feat(comments) i have added the ability to make comments from the page and i have been trying to style the elements to the center

--- game_sessions/views.py
+++ game_sessions/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,get_object_or_404
 from django.views import generic
 from .models import Post
+from .forms import CommentForm
 
 
 
@@ -20,6 +21,14 @@ class Game_session_List(generic.ListView):
         post = get_object_or_404(queryset, slug=post_id)
         comments = post.comments.all().order_by("-created_on")
         comment_count = post.comments.filter(approved=True).count()
+        if request.method == "POST":
+            comment_form = CommentForm(data=request.POST)
+            if comment_form.is_valid():
+                comment = comment_form.save(commit=False)
+                comment.gamer_tag = request.user
+                comment.post = post
+                comment.save()
+        comment_form = CommentForm()
 
         return render(request,
             "game_sessions/post_descripton.html",
@@ -27,6 +36,7 @@ class Game_session_List(generic.ListView):
             "post": post,
             "comments": comments,
             "comment_count": comment_count,
+            "comment_form": comment_form,
             }
     )
 
