5b90fba29aaa6cfbd2304914c7492207138f2dba
Aaron storey <aaron.storey@hotmail.co.uk>
2024-05-21 20:59:03 +0000

    Feat(Static) updated the static files

--- staticfiles/js/comments.js
+++ staticfiles/js/comments.js
@@ -3,12 +3,31 @@ const commentText = document.getElementById("id_body");
 const commentForm = document.getElementById("commentForm");
 const submitButton = document.getElementById("submitButton");
 
+
 for (let button of editButtons) {
-  button.addEventListener("click", (e) => {
-    let commentId = e.target.getAttribute("comment_id");
-    let commentContent = document.getElementById(`comment${commentId}`).innerText;
+  button.addEventListener("click", (a) => {
+    let commentId = a.target.dataset.comment_id;
+    let node = document.getElementById(`comment${commentId}`);
+
+    let commentContent = node.innerText;
     commentText.value = commentContent;
     submitButton.innerText = "Update";
-    commentForm.setAttribute("action", `edit_comment/${commentId}`);
+    commentForm.setAttribute("action", `edit_comment/${commentId}/`);
+  });
+}
+
+
+const deleteButtons = document.getElementsByClassName("btn-delete");
+var myModal = new bootstrap.Modal(document.getElementById('deleteModal'));
+console.log(deleteButtons)
+const deleteConfirm = document.getElementById("deleteConfirm");
+
+for (let button of deleteButtons) {
+  button.addEventListener("click", (a) => {
+  
+    console.log("delete clicked")
+    let commentId = a.target.dataset.comment_id;
+    deleteConfirm.href = `delete_comment/${commentId}`;
+    myModal.show();
   });
 }
